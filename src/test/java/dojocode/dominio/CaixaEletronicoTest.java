6f328520c3345adc03b425598d1f696e04e17b93
unknown <you@example.com>
2020-11-27 14:56:01 -0300

    resolvendo inicio do tdd

--- src/test/java/dojocode/dominio/CaixaEletronicoTest.java
+++ src/test/java/dojocode/dominio/CaixaEletronicoTest.java
@@ -8,22 +8,8 @@ import java.math.BigDecimal;
 
 public class CaixaEletronicoTest {
 
-    private BigDecimal quantidadeDeNotas;
-    private BigDecimal valorASerRetirado;
-    private Faker faker;
-    private CaixaEletronico caixaEletronico;
-
-    @BeforeEach
-    void init(){
-        faker = new Faker();
-        quantidadeDeNotas = BigDecimal.valueOf(faker.number().randomDigitNotZero());
-        valorASerRetirado = BigDecimal.valueOf(faker.number().randomDigitNotZero());
-    }
-
     @Test
-    void deve_poder_criar_um_caixa_eletronico(){
-        caixaEletronico = new CaixaEletronico(quantidadeDeNotas, valorASerRetirado);
-
-
+    void deve_criar_um_caixa_eletronico(){
+        CaixaEletronico caixaEletronico = new CaixaEletronico();
     }
 }
